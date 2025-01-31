db6e0bf268b5b4769436f1e6fda83fe99590f826
erin foley <erindagfoley@gmail.com>
2025-01-31 16:06:09 -0500

    added connection.ts code and prompts to add new employee info

--- Develop/src/index.ts
+++ Develop/src/index.ts
@@ -1,56 +1,162 @@
 import inquirer from 'inquirer';
-import pool from './index.ts';
-import 'console.table';
+//import express from 'express';
+import { pool, connectToDb } from './db/connection.js';
+await connectToDb();
 
-export const viewDepartments = async () => {
-    const result = await pool.query('SELECT * FROM department');
-    console.table(result.rows);
-};
 
-export const viewRoles = async () => {
-    const result = await pool.query(`
-        SELECT role.id, role.job_title, department.department_name AS department, role.salary 
-        FROM role 
-        JOIN department ON role.department_id = department.id
-    `);
-    console.table(result.rows);
-};
+function showMenu(){
+  inquirer.prompt([{
+    type: 'list',
+    name: 'menu',
+    message: 'What would you like to do? (use arrow keys)',
+    choices: [
+      'View all departments',
+      'View all roles',
+      'View all employees',
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'Update an employee role',
+      'Exit App'
+    ],
+  }])
+    .then((answers) => {
+      if (answers.menu === 'View all departments') {
+        pool.query('SELECT * FROM department;', (err, result) => {
+            if (err) {
+                console.error(err);
+              } else {
+                console.table(result.rows);
+                showMenu();
+              }
+        });
+      }
+      else if (answers.menu === 'View all roles') {
+        pool.query('SELECT * FROM role;', (err, result) => {
+            if (err) {
+                console.error(err);
+              } else {
+                console.table(result.rows);
+                showMenu();
+              }
+        });
+      }
+      else if (answers.menu === 'View all employees'){
+        pool.query('SELECT * FROM EMPLOYEE;', (err, result) => {
+            if (err) {
+                console.error(err);
+              } else {
+                console.table(result.rows)
+                showMenu();
+              }
+        });
+      }
+      else if (answers.menu === 'Add a department') {
+        addNewDepartment();
+      }
+      else if (answers.menu === 'Add a role') {
+        addNewRole();
+      }
+      else if (answers.menu === 'Add an employee') {
+        addNewEmployee();
+      }
+      else if (answers.menu === 'Update an employee role') {
+        updateEmployeeRole();
+      }else{
+        pool.end()
+        process.exit(0)
+      }
+    });
+}
 
-export const addEmployee = async () => {
-    const roles = await pool.query('SELECT id, job_title FROM role');
-    const managers = await pool.query('SELECT id, first_name, last_name FROM employee');
+showMenu();
 
-    const answers = await inquirer.prompt([
-        {
-            name: 'firstName',
-            message: "What is the employee's first name?"
-        },
-        {
-            name: 'lastName',
-            message: "What is the employee's last name?"
-        },
-        {
-            type: 'list',
-            name: 'roleId',
-            message: "What is the employee's role?",
-            choices: roles.rows.map(role => ({
-                name: role.job_title,
-                value: role.id
-            }))
-        },
-        {
-            type: 'list',
-            name: 'managerId',
-            message: "Who is the employee's manager?",
-            choices: managers.rows.map(manager => ({
-                name: `${manager.first_name} ${manager.last_name}`,
-                value: manager.id
-            }))
+function addNewDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter the name of the new department:',
+      },
+    ])
+    .then((response) => {
+      pool.query('INSERT INTO department (name) VALUES ($1)', [response.departmentName], (err, result) => {
+        if (err) {
+          console.error(err);
+        } else if (result) {
+          console.table(`Department ${response.departmentName} has been added`);
         }
-    ]);
+        showMenu();
+      });
+    });
+}
 
-    await pool.query(
-        'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
-        [answers.firstName, answers.lastName, answers.roleId, answers.managerId]
-    );
-};
+function addNewEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: 'Enter the first name of the new employee:',
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: 'Enter the last name of the new employee:',
+      },
+      {
+        type: 'input',
+        name: 'roleId',
+        message: 'Enter the role id of the new employee:',
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: 'Enter the manager id of the new employee:',
+      },
+    ])
+    .then((response) => {
+      pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4);',
+        [response.firstName, response.lastName, response.roleId, response.managerId], (err, result) => {
+          if (err) {
+            console.error(err);
+        } else if (result) {
+            console.log(`Employee ${response.firstName} ${response.lastName} and their role ID of ${response.roleID} and their manager ID of ${response.managerId} has been added`);
+          }
+          showMenu();
+        });
+    });
+}
+
+function addNewRole() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'roleTitle',
+        message: 'Enter the title of the new role:',
+      },
+      {
+        type: 'input',
+        name: 'roleSalary',
+        message: 'Enter the salary of the new role:',
+      },
+      {
+        type: 'input',
+        name: 'deptId',
+        message: 'Enter the department id of the new role:',
+      },
+    ])
+    .then((response) => {
+      pool.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)',
+        [response.roleTitle, response.roleSalary, response.deptId], (err, result) => {
+          if (err) {
+            console.error(err);
+        } else if (result) {
+            console.log(`Role ${response.roleTitle} with a salary of ${response.roleSalary} and a department ID of ${response.department_id} has been added`);
+          }
+          showMenu();
+        });
+    });
+}
